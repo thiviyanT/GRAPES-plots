f6c20fccea6859e88f2ed798a5a9542f8bbc8329
Thiviyan Thanapalasingam <thiviyan.t@gmail.com>
2024-05-22 14:55:22 +0200

    new results

--- compute-confidence-interval.py
+++ compute-confidence-interval.py
@@ -3,24 +3,17 @@ import numpy as np
 
 
 def compute_confidence_intervals(df):
-    """
-    Computes the 95% confidence intervals based on the mean and standard deviation in the dataframe.
-    """
-    # Computing the standard error
     df['Standard Error'] = (df['Accuracy(std)'].astype(float) / np.sqrt(df['Sampling Number'].replace('All', np.nan).astype(float))).round(2)
-    
-    # Computing the 95% confidence interval
+
     z_value = 1.96  # Assuming normal distribution
     df['CI Lower'] = (df['Accuracy(mean)'].astype(float) - z_value * df['Standard Error']).round(2)
     df['CI Upper'] = (df['Accuracy(mean)'].astype(float) + z_value * df['Standard Error']).round(2)
     
     return df
 
-
 dataframe = pd.read_csv("./results-uncertainty.csv")
 dataframe_with_ci = compute_confidence_intervals(dataframe)
 
-# Exporting the dataframe with confidence intervals to a CSV file
 output_path = "./results_with_confidence_intervals.csv"
 dataframe_with_ci.to_csv(output_path, index=False)
 
--- plot-sample-size-accuracy-uncertainty.py
+++ plot-sample-size-accuracy-uncertainty.py
@@ -1,20 +1,38 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
-# Set the style
+
+def compute_confidence_intervals(df):
+    df['Standard Error'] = (df['Accuracy(std)'].astype(float) / np.sqrt(
+        df['Sampling Number'].replace('All', np.nan).astype(float))).round(2)
+
+    z_value = 1.96  # Assuming normal distribution
+    df['CI Lower'] = (df['Accuracy(mean)'].astype(float) - z_value * df['Standard Error']).round(2)
+    df['CI Upper'] = (df['Accuracy(mean)'].astype(float) + z_value * df['Standard Error']).round(2)
+
+    return df
+
+
+dataframe = pd.read_csv("./results-uncertainty.csv")
+dataframe_with_ci = compute_confidence_intervals(dataframe)
+
+output_path = "./results_with_confidence_intervals.csv"
+dataframe_with_ci.to_csv(output_path, index=False)
+
 plt.style.use('science')
 
-# Read the corrected CSV file
 dataframe_corrected = pd.read_csv("./results_with_confidence_intervals.csv")
 
-# Remap methods and datasets
 remap_methods = {
     'ladies': 'LADIES',
     'fastgcn': 'FastGCN',
     'graphsaint': 'GraphSAINT',
-    'gsgf': 'GRAPES',
-    'asgcn': 'AS-GCN',
     'grapes': 'GRAPES',
+    'asgcn': 'AS-GCN',
+    'gfn': 'GRAPES-GFlowNet',
+    'rl': 'GRAPES-RL',
+    'random': 'Random',
 }
 
 remap_datasets = {
@@ -32,21 +50,21 @@ remap_datasets = {
     'ogbn-products': 'ogbn-products',
 }
 
-# Colors for models
-colors = {
-    'LADIES': 'black',
+colrs = {
+    'LADIES': 'blue',
     'FastGCN': 'green',
     'GraphSAINT': 'orange',
-    'GRAPES': '#4040FF',
     'AS-GCN': 'red',
+    'GRAPES-GFlowNet': '#9EF8EE',
+    'GRAPES-RL': '#45214A',
+    'Random': '#747E7E',
 }
 
-# Create a structured data dictionary from the corrected dataframe
 data = {}
 for index, row in dataframe_corrected.iterrows():
     model = remap_methods[row['Method'].lower()]
     dataset = remap_datasets[row['Dataset'].lower()]
-    sample = row['Sampling Number']
+    smaple = row['Sampling Number']
     accuracy = float(row['Accuracy(mean)'])
     ci_lower = float(row['CI Lower'])
     ci_upper = float(row['CI Upper'])
@@ -56,47 +74,64 @@ for index, row in dataframe_corrected.iterrows():
     if dataset not in data[model]:
         data[model][dataset] = {}
 
-    data[model][dataset][sample] = (accuracy, ci_lower, ci_upper)
+    data[model][dataset][smaple] = (accuracy, ci_lower, ci_upper)
 
-# Ordered datasets
-ordered_datasets = ['Cora', 'Citeseer', 'Pubmed', 'Reddit', 'Flickr', 'Yelp', 'ogbn-arxiv', 'ogbn-products']
+ordered_datasets = ['Reddit', 'Flickr', 'Yelp', 'ogbn-arxiv']
 
-# Plotting
-fig, axes = plt.subplots(2, 4, figsize=(12, 6))
+fig, axes = plt.subplots(1, 4, figsize=(12, 3))
 for ax, dataset in zip(axes.flatten(), ordered_datasets):
-    for model, datasets in data.items():
-        if dataset in datasets:
+    for model in ['LADIES', 'FastGCN', 'GraphSAINT', 'AS-GCN', 'Random']:
+        if dataset in data.get(model, {}):
+            sorted_samples = sorted(data[model][dataset].keys())
             x = []
             y = []
             lower_bound = []
             upper_bound = []
-            for sample, values in datasets[dataset].items():
+            for sample in sorted_samples:
+                values = data[model][dataset][sample]
                 x.append(sample)
                 y.append(values[0])
                 lower_bound.append(values[1])
                 upper_bound.append(values[2])
-            ax.plot(x, y, label=model, marker='o', color=colors[model], linewidth=2.0)
-            ax.fill_between(x, lower_bound, upper_bound, color=colors[model], alpha=0.2)
+            ax.plot(x, y, label=model, marker='o', color=colrs[model], linewidth=2.0, linestyle='dotted')
+            ax.fill_between(x, lower_bound, upper_bound, color=colrs[model], alpha=0.04)
 
-    ax.set_title(f"{dataset}", fontsize=20, fontweight='bold')
+    for model in ['GRAPES-RL', 'GRAPES-GFlowNet']:
+        if dataset in data.get(model, {}):
+            sorted_samples = sorted(data[model][dataset].keys())
+            x = []
+            y = []
+            lower_bound = []
+            upper_bound = []
+            for sample in sorted_samples:
+                values = data[model][dataset][sample]
+                x.append(sample)
+                y.append(values[0])
+                lower_bound.append(values[1])
+                upper_bound.append(values[2])
+            ax.plot(x, y, label=model, marker='o', color=colrs[model], linewidth=2.0, linestyle='solid')
+            ax.fill_between(x, lower_bound, upper_bound, color=colrs[model], alpha=0.04)
+
+    ax.set_title(f"{dataset}", fontsize=15, fontweight='bold')
     if dataset == 'Reddit':
-        ax.legend()
-    ax.grid(True)
+        ax.legend(fontsize=8)
     ax.set_xticks([32, 64, 128, 256, 512])
     ax.set_xticklabels([32, 64, 128, 256, 512], rotation=90, fontsize=10)
-    ax.set_ylim(0, 100)
-
-    # Set the x-axis to be logarithmic with base 2
     ax.set_xscale('log', base=2)
 
+    # # Adjust the spines to create a gap
+    # # Hide the top and right spines
+    # ax.spines['top'].set_visible(False)
+    # ax.spines['right'].set_visible(False)
+    #
+    # # Move the bottom and left spines away from zero
+    # ax.spines['bottom'].set_position(('outward', 10))  # Move bottom spine down
+    # ax.spines['left'].set_position(('outward', 10))  # Move left spine left
+
 plt.tight_layout()
-
-# Padding around the figure
 plt.subplots_adjust(left=0.05, right=0.99, top=0.94, bottom=0.1)
-
-# Add a common y-axis label and x-axis label to the entire grid of subplots
 fig.text(0.01, 0.5, 'Node Classification F1-scores', va='center', rotation='vertical', fontsize=15, fontweight='bold')
-fig.text(0.5, 0.02, 'Sample Size (logarithmic base-2)', ha='center', fontsize=15, fontweight='bold')
+fig.text(0.5, -0.05, 'Sample Size (logarithmic base-2)', ha='center', fontsize=15, fontweight='bold')
 
 plt.savefig("sampling_vs_accuracy_confidence_intervals.pdf", format='pdf', bbox_inches='tight')
 plt.show()
